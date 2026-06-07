53e068c95c18349258812d44f515f248c8dca5a7
RushiRuns <rushikeshaundhakar715@gmail.com>
2026-06-07 10:43:30 +0530

    design: applied appropriete background colors for the area of top bar and navigation bar.

--- app/src/main/java/com/rushi/sentinel/ui/theme/Theme.kt
+++ app/src/main/java/com/rushi/sentinel/ui/theme/Theme.kt
@@ -1,8 +1,13 @@
 package com.rushi.sentinel.ui.theme
 
+import android.app.Activity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
     primary = PrimaryTeal,
@@ -20,6 +25,19 @@ private val DarkColorPalette = darkColors(
 
 @Composable
 fun SentinelTheme(content: @Composable () -> Unit) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = DeepBackground.toArgb()
+            window.navigationBarColor = DeepBackground.toArgb()
+            
+            val insetsController = WindowCompat.getInsetsController(window, view)
+            insetsController.isAppearanceLightStatusBars = false
+            insetsController.isAppearanceLightNavigationBars = false
+        }
+    }
+
     MaterialTheme(
         colors = DarkColorPalette,
         typography = Typography,
